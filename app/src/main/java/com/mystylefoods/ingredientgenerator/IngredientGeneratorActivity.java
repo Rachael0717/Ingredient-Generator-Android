70a798ea48abd723c52f00b658643fa4c0175356
Rachael Paul <rachael0717@gmail.com>
2019-01-05 21:49:50 -0800

    Adds OnClickListener to generate button

--- app/src/main/java/com/mystylefoods/ingredientgenerator/IngredientGeneratorActivity.java
+++ app/src/main/java/com/mystylefoods/ingredientgenerator/IngredientGeneratorActivity.java
@@ -2,13 +2,31 @@ package com.mystylefoods.ingredientgenerator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class IngredientGeneratorActivity extends Activity {
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private Button generateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_selection);
+
+        generateButton = findViewById(R.id.generate_button);
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGenerateClicked();
+            }
+        });
+    }
+
+    private void onGenerateClicked() {
+        Log.i("Test", "The button was clicked!");
     }
 
 }
