bb9beab8c8d7e0284a1fedd2c2ed4298f246852b
Rachael Paul <rachael0717@gmail.com>
2019-01-05 21:07:44 -0800

    Adds Ingredient Generator Activity

---
+++ app/src/main/java/com/mystylefoods/ingredientgenerator/IngredientGeneratorActivity.java
@@ -0,0 +1,14 @@
+package com.mystylefoods.ingredientgenerator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IngredientGeneratorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_selection);
+    }
+
+}
